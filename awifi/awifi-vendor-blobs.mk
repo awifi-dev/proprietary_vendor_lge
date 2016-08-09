3801fd0f8ff828fdc88283e6198409a338a0bc2a
fefifofum <jorsellis@gmail.com>
2016-08-10 01:11:20 +0200

    Add libuiblur.so

--- awifi/awifi-vendor-blobs.mk
+++ awifi/awifi-vendor-blobs.mk
@@ -172,7 +172,8 @@ PRODUCT_COPY_FILES += \
     vendor/lge/awifi/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     vendor/lge/awifi/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
     vendor/lge/awifi/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
-    vendor/lge/awifi/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so
+    vendor/lge/awifi/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
+    vendor/lge/awifi/proprietary/vendor/lib/libuiblur.so:system/vendor/lib/libuiblur.so
 
 # Perf (V500-30B)
 PRODUCT_COPY_FILES += \
